e2440b91fc05da6eb8df071cad3614bc2e63c775
Daniela G. Miranda <140209051+DanielaMiranda22@users.noreply.github.com>
2024-06-04 13:45:56 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+//Función mostrar y ocultar info //
+
+function muestra_oculta(id){
+    let div = document.getElementById(id);
+    if (div.style.display == "flex"){
+        div.style.display = "none";
+    }
+    else {
+        div.style.display ="flex";
+    }
+}
+
+//Cambio de página
+let codigo = "AlumnoSC";
+
+function code(){
+    respuesta =document.getElementById("answer").value;
+    console.log(respuesta);
+    document.getElementById("answer").value = "";
+    if(codigo==respuesta){
+        location.href = "main.html"
+    } else {
+        document.getElementById("verifica").innerHTML = "Inténtalo nuevamente"
+        document.getElementById("verifica").style.color = "red";
+    }
+}
